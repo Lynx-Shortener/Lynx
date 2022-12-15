4b5625ff58212b56d13cf4d3e0fa63edb189674e
Jack Bailey <jack@jackbailey.dev>
2022-12-15 18:30:41 +0000

    Remove uploaded file if fields fail

--- src/api/middleware/requireFields.js
+++ src/api/middleware/requireFields.js
@@ -1,7 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = (fields, fieldLocation) => {
 	return function (req, res, next) {
 		const unmetFields = fields.filter((field) => !Object.keys(req[fieldLocation || "body"]).includes(field));
 		if (unmetFields.length !== 0) {
+			if (req.file) {
+				fs.unlinkSync(path.join("tmp", "uploads", req.file.filename));
+			}
 			return res.status(400).json({
 				success: false,
 				message: `The following required fields were not included in the request: ${unmetFields.join(", ")}`,
