6d26771cf27cacad479ed216907ef3392d737661
Jack Bailey <jack@jackbailey.dev>
2022-12-16 16:06:20 +0000

    Revamp sidebar

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,24 +1,22 @@
 <template>
 	<div class="sidebar">
-		<div class="upperIcons">
-			<router-link to="/dash/overview">
-				<font-awesome-icon icon="list" />
-				<p>Overview</p>
-			</router-link>
-			<router-link to="/dash/settings">
-				<font-awesome-icon icon="gear" />
-				<p>Settings</p>
-			</router-link>
-		</div>
-		<div class="lowerIcons">
-			<a @click="darkMode.toggle()" class="darkmode">
-				<font-awesome-icon :icon="darkMode.dark ? 'sun' : 'moon'" />
-				<p>{{ darkMode.dark ? "Light Mode" : "Dark Mode" }}</p>
-			</a>
-			<a class="logout">
-				<font-awesome-icon icon="right-from-bracket" />
-				<p>Logout</p>
-			</a>
+		<div class="sidebarContent">
+			<div class="upperIcons">
+				<router-link to="/dash/overview">
+					<font-awesome-icon icon="list" />
+				</router-link>
+				<router-link to="/dash/settings">
+					<font-awesome-icon icon="gear" />
+				</router-link>
+			</div>
+			<div class="lowerIcons">
+				<a @click="darkMode.toggle()" class="darkmode">
+					<font-awesome-icon :icon="darkMode.dark ? 'sun' : 'moon'" />
+				</a>
+				<a class="logout">
+					<font-awesome-icon icon="right-from-bracket" />
+				</a>
+			</div>
 		</div>
 	</div>
 </template>
@@ -36,59 +34,68 @@ export default {
 
 <style lang="scss" scoped>
 .sidebar {
-	background: var(--bg-color-2);
-	height: 100%;
-	position: relative;
-	width: max-content;
-	padding: 1.5rem 1.5rem;
-	display: flex;
-	flex-direction: column;
-	box-sizing: border-box;
 	height: 100vh;
-	-webkit-user-select: none; /* Safari */
-	-ms-user-select: none; /* IE 10 and IE 11 */
-	user-select: none; /* Standard syntax */
-	justify-content: space-between;
-	.upperIcons {
+	padding: 1rem;
+	box-sizing: border-box;
+	.sidebarContent {
+		background: var(--accent);
+		height: 100%;
+		width: 100%;
+		border-radius: 10px;
+		padding: 2rem 1rem;
+		box-sizing: border-box;
 		display: flex;
 		flex-direction: column;
-		gap: 0.5rem;
-	}
-	a {
-		text-decoration: none;
-		color: inherit;
-		display: flex;
-		gap: 1rem;
-		align-items: center;
-		padding: 0.4rem 0.5rem;
-		border-radius: 2px;
-		cursor: pointer;
-		white-space: nowrap;
-		&.router-link-exact-active {
-			background: var(--accent);
-			color: var(--accent-color);
+		justify-content: space-between;
+
+		.upperIcons,
+		.lowerIcons {
+			display: flex;
+			flex-direction: column;
+			gap: 0.5rem;
+			a {
+				color: var(--accent-color-inactive);
+				font-size: 1.4rem;
+				padding: 0.5rem;
+				box-sizing: border-box;
+				cursor: pointer;
+				transition: 250ms ease-in-out;
+				border-radius: 5px;
+				&.router-link-exact-active {
+					background: var(--accent-hover);
+					color: var(--accent-color);
+				}
+
+				&:hover {
+					background: var(--accent-hover);
+					color: var(--accent-color);
+					&.router-link-exact-active {
+						background: var(--accent-hover-active);
+					}
+				}
+			}
 		}
-		&.logout,
-		&.darkmode {
-			svg {
-				background: var(--accent);
-				padding: 10px;
-				border-radius: 50%;
+
+		.lowerIcons {
+			a {
 				color: var(--accent-color);
-				aspect-ratio: 1/1;
 			}
 		}
 	}
-	.spacer {
-		flex-grow: 1;
-	}
 
 	@media screen and (max-width: 768px) {
-		flex-direction: row;
 		width: 100%;
 		height: max-content;
-		align-items: center;
-		padding: 1rem 1.5rem 1rem;
+		padding: 0;
+		.sidebarContent {
+			flex-direction: row;
+			padding: 1rem;
+			border-radius: 0;
+			.upperIcons,
+			.lowerIcons {
+				flex-direction: row;
+			}
+		}
 		a {
 			height: max-content;
 			p {
