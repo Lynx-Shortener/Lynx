0d6e7f63a63d915e7074730a9cbbdf60641e03d0
Jack Bailey <jack.bailey@rfa.co.uk>
2023-04-20 13:52:22 +0100

    Fix crash on failure to authenticate with token

--- src/db/modules/account/get/bySecret.js
+++ src/db/modules/account/get/bySecret.js
@@ -2,5 +2,7 @@ const Account = require("../../../models/account");
 
 module.exports = async ({ secret }) => {
 	const account = await Account.findOne({ secret });
-	return [account, !account];
+	if (account) return [account, null];
+
+	return [null, { code: 401, message: "Invalid secret" }];
 };
