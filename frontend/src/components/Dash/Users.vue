eed9cac1f08d4fb280e69b7039ea8548e3984ab7
Jack Bailey <jack.bailey@rfa.co.uk>
2023-05-02 10:05:56 +0100

    Fix fetching and add start of table layout

--- frontend/src/components/Dash/Users.vue
+++ frontend/src/components/Dash/Users.vue
@@ -1,7 +1,24 @@
 <template>
 	<div class="users">
 		<h2>User Management</h2>
-		{{ users }}
+		<table>
+			<thead>
+				<th>Username</th>
+				<th>Email</th>
+				<th>Role</th>
+				<th>Secret</th>
+				<th>Action</th>
+			</thead>
+			<tbody>
+				<tr v-for="user in users" :key="user.id">
+					<td>{{ user.username }}</td>
+					<td>{{ user.email }}</td>
+					<td>{{ user.role }}</td>
+					<tr>{{ user.secret }}</tr>
+					<td class="actions"><font-awesome-icon icon="check"/></td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
 </template>
 
@@ -18,7 +35,7 @@ export default {
 	},
 	methods: {
 		async getUsers() {
-			const userResponse = await this.account.fetch("/user/list");
+			const userResponse = await this.account.fetch("/user/list", {});
 			if (!userResponse.success) {
 				this.popups.addPopup("Information", {
 					title: "Error getting list of users",
@@ -48,196 +65,44 @@ export default {
 </script>
 
 .<style lang="scss" scoped>
-.settings {
+.users {
 	padding-block: 2rem;
 	text-align: left;
 	display: flex;
 	flex-direction: column;
 	gap: 2rem;
 
-	> div {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-		h2 {
-			font-size: 1.5rem;
-			font-weight: 500;
-			color: var(--color-2);
-		}
-		p {
-			color: var(--color-3);
-			font-weight: 300;
-		}
-		.inputs {
-			margin-top: 2rem;
-			display: flex;
-			flex-direction: column;
-			gap: 2rem;
-			width: max-content;
-			.input {
-				&.totp {
-					p {
-						margin-bottom: 0.5rem;
-					}
-				}
-				&:not(.secret) {
-					width: 100%;
-					div {
-						border: 1px solid var(--bg-color-2);
-						padding: 0.5rem;
-						border-radius: 5px;
-						min-width: 10rem;
-						cursor: pointer;
-						&[disabled="true"] {
-							opacity: 0.5;
-							cursor: not-allowed !important;
-						}
-					}
-				}
-				&.secret {
-					> div {
-						display: flex;
-						flex-wrap: row;
-						align-items: center;
-						border: 1px solid var(--bg-color-2);
-						border-radius: 5px;
-						width: max-content;
-						margin-top: 1rem;
-						height: max-content;
-						overflow: hidden;
+	h2 {
+		font-size: 2rem;
+		font-weight: 500;
+	}
 
-						p {
-							padding: 0.5rem;
-							font-family: monospace;
-							box-sizing: border-box;
-							margin: 0;
-							height: max-content;
-							line-height: 1;
-							width: calc(32ch + 1rem);
-						}
-						.actions {
-							display: flex;
-							align-items: center;
-							> div {
-								&[disabled="true"] {
-									opacity: 0.5;
-									cursor: not-allowed !important;
-								}
-								padding: 0.5rem;
-								display: grid;
-								place-content: center;
-								box-sizing: border-box;
-								cursor: pointer;
-								width: 2rem;
-								color: var(--accent-color);
-								&.view {
-									background: var(--accent);
-									&:hover {
-										background: var(--accent-hover);
-									}
-								}
-								&.copy {
-									background: #3ca4e9;
-									&:hover {
-										background: lighten(#3ca4e9, 5%);
-									}
-								}
-								&.new {
-									background: var(--color-error);
-									&:hover {
-										background: var(--color-error-hover);
-									}
-									svg {
-										&[spinning="true"] {
-											animation: spin 1s linear infinite;
-											@keyframes spin {
-												from {
-													transform: rotate(0deg);
-												}
-												to {
-													transform: rotate(180deg);
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
+	table {
+		width: 100%;
+		thead {
+			color: var(--accent-color);
+			th {
+				background: var(--accent);
+				padding: 0.8rem;
+				&:first-of-type {
+					border-top-left-radius: 10px;
+					border-bottom-left-radius: 10px;
 				}
-				> button {
-					background: var(--accent);
-					width: max-content;
-					padding: 0.5rem 1rem;
-					color: var(--accent-color);
-					border: none;
-					border-radius: 5px;
-					font: inherit;
-					cursor: pointer;
-					font-size: 1rem;
-					&[disabled] {
-						opacity: 0.5;
-						cursor: not-allowed;
-					}
+				&:last-of-type {
+					border-top-right-radius: 10px;
+					border-bottom-right-radius: 10px;
 				}
-				label {
-					font-weight: 500;
-					font-size: 0.9rem;
-					margin-bottom: 1rem;
-					display: block;
-				}
+			}
+		}
+		tbody {
+			td {
+				padding: 0.8rem;
 			}
 		}
 	}
 
 	@media screen and (max-width: 768px) {
 		padding-inline: 2rem;
-		> div {
-			h2 {
-				font-size: 3rem;
-			}
-			p {
-				font-size: 1.5rem;
-			}
-
-			.inputs {
-				width: 100%;
-				.input {
-					label {
-						font-size: 1.1rem;
-						line-height: 1.5;
-					}
-					div {
-						font-size: 1.2rem;
-					}
-					&.secret {
-						width: 100%;
-						> div {
-							width: 100%;
-							flex-direction: column;
-							box-sizing: border-box;
-							align-items: flex-start;
-							p {
-								font-size: 1.3rem;
-								text-align: left;
-							}
-							.actions {
-								display: grid;
-								grid-template-columns: repeat(3, 1fr);
-								width: 100%;
-								.action {
-									width: 100%;
-								}
-							}
-						}
-					}
-					button {
-						padding: 1rem 1.5rem;
-						font-size: 1.2rem;
-					}
-				}
-			}
-		}
 	}
 }
 </style>
