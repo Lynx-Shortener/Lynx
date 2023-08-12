06074e7f7a62b0d1ed3f37839cda878d7d7ab230
Jack Bailey <81699395+JackBailey@users.noreply.github.com>
2023-08-12 11:57:36 +0100

    Fix eslint format settings

--- src/modules/backup.js
+++ src/modules/backup.js
@@ -4,8 +4,8 @@ const Link = require("../db/models/link");
 module.exports = async ({ count: backupCount }) => {
     const links = await Link.find({}, "-_id -__v");
     if (!fs.existsSync("../backups")) {
-		fs.mkdirSync("../backups");
-	}
+        fs.mkdirSync("../backups");
+    }
 
     let filename = "backup";
 
@@ -15,21 +15,21 @@ module.exports = async ({ count: backupCount }) => {
 
     fs.writeFileSync(`../backups/${filename}.json`, JSON.stringify(links, null, 4));
 
-	if (backupCount !== -1) {
-		const existingBackups = fs.readdirSync("../backups").map((file) => ({
-			filename: file,
-			time: fs.statSync(`../backups/${file}`).mtime.getTime(),
-		}));
+    if (backupCount !== -1) {
+        const existingBackups = fs.readdirSync("../backups").map((file) => ({
+            filename: file,
+            time: fs.statSync(`../backups/${file}`).mtime.getTime(),
+        }));
 
-		const oldBackups = existingBackups
-			.sort((a, b) => a.time - b.time)
-			.map((file) => file.filename)
-			.slice(0, backupCount * -1);
+        const oldBackups = existingBackups
+            .sort((a, b) => a.time - b.time)
+            .map((file) => file.filename)
+            .slice(0, backupCount * -1);
 
-		oldBackups.forEach((file) => {
-			fs.unlinkSync(`../backups/${file}`);
-		});
-	}
+        oldBackups.forEach((file) => {
+            fs.unlinkSync(`../backups/${file}`);
+        });
+    }
 
     console.log(`${filename}.json created. Links backed up: ${links.length.toLocaleString()}`);
 };
