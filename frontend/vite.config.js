f04dbe7af2fd792e0c7503877c4e2ad679c9887c
Jack Bailey <81699395+JackBailey@users.noreply.github.com>
2024-03-26 10:13:55 +0000

    Remove vite-plugin-rewrite-all

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import pluginRewriteAll from "vite-plugin-rewrite-all";
+// import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), pluginRewriteAll()],
+    plugins: [vue()],
     css: {
         preprocessorOptions: {
             scss: {
