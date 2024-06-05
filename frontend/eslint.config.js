46f2d0aa7909ac5a7b2e714b3c39f2ea15a2c769
Jack Bailey <81699395+JackBailey@users.noreply.github.com>
2024-06-05 11:07:44 +0100

    Use new eslint format

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,64 +1,66 @@
-module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        "airbnb-base",
-        "plugin:vue/vue3-essential",
-    ],
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: [
-                ".eslintrc.{js,cjs}",
-                "",
-            ],
-            parserOptions: {
-                sourceType: "script",
-            },
+export default [
+    {
+        env: {
+            browser: true,
+            es2021: true,
         },
-    ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    plugins: [
-        "vue",
-    ],
-    rules: {
-        "func-names": "off",
-        "no-prototype-buildins": "off",
-        "no-console": "off",
-        "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
-        indent: ["error", 4],
-        "no-shadow": ["error", { ignoreOnInitialization: true }],
-        "max-len": ["error", {
-            code: 200,
-        }],
-        "import/prefer-default-export": ["off"],
-        "vue/multi-word-component-names": 0,
-        "no-param-reassign": ["error", { props: false }],
-        "consistent-return": 0,
-        "vue/singleline-html-element-content-newline": ["error", {
-            ignoreWhenNoAttributes: true,
-            ignoreWhenEmpty: true,
-        }],
-        "vue/max-attributes-per-line": ["error", {
-            singleline: {
-                max: 3,
+        extends: [
+            "airbnb-base",
+            "plugin:vue/vue3-essential",
+        ],
+        overrides: [
+            {
+                env: {
+                    node: true,
+                },
+                files: [
+                    ".eslintrc.{js,cjs}",
+                    "",
+                ],
+                parserOptions: {
+                    sourceType: "script",
+                },
             },
-            multiline: {
-                max: 1,
-            },
-        }],
-        "vue/html-indent": ["error", 4, {
-            alignAttributesVertically: true,
-        }],
-        "vue/no-v-html": "off",
-        quotes: ["error", "double"],
-        semi: ["error", "always"],
-    },
-};
+        ],
+        parserOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+        plugins: [
+            "vue",
+        ],
+        rules: {
+            "func-names": "off",
+            "no-prototype-buildins": "off",
+            "no-console": "off",
+            "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+            indent: ["error", 4],
+            "no-shadow": ["error", { ignoreOnInitialization: true }],
+            "max-len": ["error", {
+                code: 200,
+            }],
+            "import/prefer-default-export": ["off"],
+            "vue/multi-word-component-names": 0,
+            "no-param-reassign": ["error", { props: false }],
+            "consistent-return": 0,
+            "vue/singleline-html-element-content-newline": ["error", {
+                ignoreWhenNoAttributes: true,
+                ignoreWhenEmpty: true,
+            }],
+            "vue/max-attributes-per-line": ["error", {
+                singleline: {
+                    max: 3,
+                },
+                multiline: {
+                    max: 1,
+                },
+            }],
+            "vue/html-indent": ["error", 4, {
+                alignAttributesVertically: true,
+            }],
+            "vue/no-v-html": "off",
+            quotes: ["error", "double"],
+            semi: ["error", "always"],
+        },
+    }
+]
--- src/eslint.config.js
+++ src/eslint.config.js
@@ -1,41 +1,43 @@
-module.exports = {
-    env: {
-        node: true,
-        es2021: true,
-    },
-    extends: [
-        "airbnb-base",
-    ],
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: [
-                ".eslintrc.{js,cjs}",
-            ],
-            parserOptions: {
-                sourceType: "script",
-            },
+export default [
+    {
+        env: {
+            node: true,
+            es2021: true,
         },
-    ],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    rules: {
-        "global-require": "off",
-        "no-prototype-buildins": "off",
-        "no-console": "off",
-        indent: ["error", 4],
-        "no-shadow": ["error", { ignoreOnInitialization: true }],
-        "max-len": ["error", {
-            code: 200,
-        }],
-        "import/prefer-default-export": ["off"],
-        "consistent-return": 0,
-        "no-param-reassign": ["error", { props: false }],
-        quotes: ["error", "double"],
-        semi: ["error", "always"],
-    },
-};
+        extends: [
+            "airbnb-base",
+        ],
+        overrides: [
+            {
+                env: {
+                    node: true,
+                },
+                files: [
+                    ".eslintrc.{js,cjs}",
+                ],
+                parserOptions: {
+                    sourceType: "script",
+                },
+            },
+        ],
+        parserOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+        rules: {
+            "global-require": "off",
+            "no-prototype-buildins": "off",
+            "no-console": "off",
+            indent: ["error", 4],
+            "no-shadow": ["error", { ignoreOnInitialization: true }],
+            "max-len": ["error", {
+                code: 200,
+            }],
+            "import/prefer-default-export": ["off"],
+            "consistent-return": 0,
+            "no-param-reassign": ["error", { props: false }],
+            quotes: ["error", "double"],
+            semi: ["error", "always"],
+        },
+    }
+]
